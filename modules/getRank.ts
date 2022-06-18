7846d698baf82e68d8e1d97ac54fced7c1a73383
Yinan Li <gabriel.li5566@gmail.com>
2022-06-19 02:43:55 +0300

    Optimize rank methods

--- modules/getRank.ts
+++ modules/getRank.ts
@@ -25,7 +25,7 @@ async function getRank(req: Request, res: Response): Promise<void> {
   //     (item: IMeasurement) => item.deviceProps.type === DeviceNames[itemNo]
   //   )
   //   .map((item: any) => item.properties[property])
-  // console.log(`Top 10: ${getMaxes(newArray, 10)}`) // hoisting
+  // console.log(`Top 10: ${getMaxes(newArray, 10)}`)
   // 1M.json: 99999,99999,99999,99999,99999,99998,99998,99998,99998,99998 (≈ under 1 second)
 
   //* Solution 2 - Using stream
@@ -34,37 +34,60 @@ async function getRank(req: Request, res: Response): Promise<void> {
   // Disadvantage:
   // Using the stream solution for solving 1M.json is much slower than using array methods.
 
+  // const fs = require('fs')
+  // const JSONStream = require('JSONStream')
+  // const readable = fs.createReadStream(dataPath, {
+  //   encoding: 'utf8',
+  // })
+  // const newArray: Array<number> = []
+  // const parser = JSONStream.parse('.')
+  // readable
+  //   .on('end', () => {
+  //     // console.log(`Top 10: ${getMaxes(newArray, 10)}`)
+  //     res.send(`Top 10: ${getMaxes(newArray, 10)}`)
+  //     // 1M.json: 99999,99999,99999,99999,99999,99998,99998,99998,99998,99998 (≈ 40 seconds)
+  //     // 10M.json: 99999,99999,99999,99999,99999,99999,99999,99999,99999,99999 (≈ 6 min 50 seconds)
+  //   })
+  //   .pipe(parser)
+  // parser.on('data', (data: any) => {
+  //   if (data.deviceProps.type === DeviceNames[itemNo]) {
+  //     newArray.push(data.properties[property])
+  //   }
+  // })
+
+  // function getMaxes(input: Array<number>, count: number) {
+  //   let output: Array<number> = []
+  //   for (let i = 0; i < input.length; i++) {
+  //     output.push(input[i])
+  //     if (output.length > count) {
+  //       output.sort((a, b) => b - a)
+  //       output.pop()
+  //     }
+  //   }
+  //   return output
+  // }
+
+  let rankArray = new Array(10).fill(0)
   const fs = require('fs')
   const JSONStream = require('JSONStream')
   const readable = fs.createReadStream(dataPath, {
     encoding: 'utf8',
+    // highWaterMark:
   })
-  const newArray: Array<number> = []
   const parser = JSONStream.parse('.')
   readable
     .on('end', () => {
-      console.log(`Top 10: ${getMaxes(newArray, 10)}`)
-      // 1M.json: 99999,99999,99999,99999,99999,99998,99998,99998,99998,99998 (≈ 40 seconds)
-      // 10M.json: 99999,99999,99999,99999,99999,99999,99999,99999,99999,99999 (≈ 6 min 50 seconds)
+      res.send(rankArray)
     })
     .pipe(parser)
-  parser.on('data', (data: any) => {
+  parser.on('data', (data: { [key: string]: { [key: string]: string } }) => {
     if (data.deviceProps.type === DeviceNames[itemNo]) {
-      newArray.push(data.properties[property])
-    }
-  })
-
-  function getMaxes(input: Array<number>, count: number) {
-    let output: Array<number> = []
-    for (let i = 0; i < input.length; i++) {
-      output.push(input[i])
-      if (output.length > count) {
-        output.sort((a, b) => b - a)
-        output.pop()
+      rankArray.sort((a, b) => b - a)
+      if (data.properties[property] > rankArray[rankArray.length - 1]) {
+        rankArray[rankArray.length - 1] = data.properties[property]
       }
     }
-    return output
-  }
+  })
 }
 
 export default getRank
